6707c57546d09a6a9d04e0c65a27c67724c755fe
Soumya Gupta <soumyagupta2304@gmail.com>
2025-08-30 13:22:55 +0530

    Revert "mudra upload"
    This reverts commit 67e5718576d2c371105fe44d32eb721c6e15f44d.

--- FSJ_MPR/script.js
+++
@@ -1,6 +0,0 @@
-const ham = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.nav-links');
-
-ham.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
